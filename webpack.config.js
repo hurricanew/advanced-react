6952343a335f5a59b8f7bb0933c236adbc70bf16
Lucas Wang <lucas@lucass-mbp-2.home>
2018-11-28 08:25:21 +1100

    feat(adReact): add jest tests and refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 var path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: ['babel-polyfill','./lib/components/Index.js'],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        // configuration regarding modules
-        rules: [
-            // rules for modules (configure loaders, parser options, etc.)
-            {
-                test: /\.js$/, exclude: /node_modules/,use: 'babel-loader'
-            }
+  mode: 'development',
+  entry: ['babel-polyfill','./lib/renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+      {
+        test: /\.js$/, exclude: /node_modules/,use: 'babel-loader'
+      }
 
-        ]
-    }
+    ]
+  }
 };
